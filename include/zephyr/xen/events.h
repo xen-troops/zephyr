87a02cc2cb6a9b38fa2d2fe626d66d03d724942f
Mykola Perehudov <Mykola_Perehudov@epam.com>
2022-09-30 14:31:41 +0300

    xenvm: update event channel driver function signatures and header
    Authored-by: Mykola Perehudov <Mykola_Perehudov@epam.com>
    Signed-off-by: Mykola Perehudov <Mykola_Perehudov@epam.com>
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- include/zephyr/xen/events.h
+++ include/zephyr/xen/events.h
@@ -26,7 +26,7 @@ typedef struct event_channel_handle evtchn_handle_t;
 int evtchn_status(evtchn_status_t *status);
 int evtchn_close(evtchn_port_t port);
 int evtchn_set_priority(evtchn_port_t port, uint32_t priority);
-void notify_evtchn(evtchn_port_t port);
+int notify_evtchn(evtchn_port_t port);
 
 /*
  * Allocate event-channel between caller and remote domain
@@ -67,6 +67,9 @@ int bind_event_channel(evtchn_port_t port, evtchn_cb_t cb, void *data);
  */
 int unbind_event_channel(evtchn_port_t port);
 int get_missed_events(evtchn_port_t port);
+
+int unmask_event_channel(evtchn_port_t port);
+void clear_event_channel(evtchn_port_t port);
 
 int xen_events_init(void);
 
