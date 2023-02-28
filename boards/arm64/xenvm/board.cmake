339fb043ce7ff365041980e2c7c3d8a6cf2d0b9e
Dmytro Semenets <dmytro_semenets@epam.com>
2023-02-28 21:56:10 +0200

    xenvm-qemu: run qemu with proper gic version
    This is trmporary solution and can be removed when build will be
    performed with moulin
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- boards/arm64/xenvm/board.cmake
+++ boards/arm64/xenvm/board.cmake
@@ -3,13 +3,18 @@ set(QEMU_ARCH aarch64)
 
 set(QEMU_CPU_TYPE_${ARCH} cortex-a57)
 
+if(CONFIG_XENVM_USE_GIC_V3)
+    set(QEMU_GIC_VERSION 3)
+else()
+    set(QEMU_GIC_VERSION 2)
+endif()
 set(QEMU_KERNEL_OPTION -kernel ${ZEPHYR_BINARY_DIR}/xen)
 set(QEMU_FLAGS_${ARCH}
    -gdb tcp::4224
    -cpu ${QEMU_CPU_TYPE_${ARCH}} -m 6G
    -nographic
-   -machine virt,gic-version=2,virtualization=true
+   -machine virt,gic-version=${QEMU_GIC_VERSION},virtualization=true
    -device loader,file=${ZEPHYR_BINARY_DIR}/zephyr.bin,addr=0x40600000
-   -dtb ${ZEPHYR_BINARY_DIR}/virt_gicv2.dtb
+   -dtb ${ZEPHYR_BINARY_DIR}/virt_gicv${QEMU_GIC_VERSION}.dtb
 )
 
