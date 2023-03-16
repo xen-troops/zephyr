d1170687988f97fb13df729a7271c045fb64cdad
Mykola Kvach <mykola_kvach@epam.com>
2023-03-16 13:12:43 +0200

    soc: arm64: rcar: mmu: add mapping of Hypervisor mem region
    Add a MMU mapping of Xen Hypervisor memory. It is needed for
    using Rcar Gen3 SoC for Xen Initial domain.
    
    Note: mapping will occur only in case if node present in dts
          ant it has 'okay' status.
    
    Todo: add mapping to a init driver function or use SYS_INIT
          for it in case if driver init function doesn't exist.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- soc/arm64/renesas_rcar/gen3/mmu_regions.c
+++ soc/arm64/renesas_rcar/gen3/mmu_regions.c
@@ -23,6 +23,13 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_INST(0, renesas_r8a7795_cpg_mssr)),
 			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
 
+#if DT_NODE_HAS_STATUS(DT_INST(0, xen_xen), okay)
+	/* TODO: add mapping to a init driver function or use SYS_INIT for mapping this region */
+	MMU_REGION_FLAT_ENTRY("HYPERVISOR",
+			      DT_REG_ADDR_BY_IDX(DT_INST(0, xen_xen), 0),
+			      DT_REG_SIZE_BY_IDX(DT_INST(0, xen_xen), 0),
+			      MT_NORMAL | MT_P_RW_U_NA | MT_NS),
+#endif
 };
 
 const struct arm_mmu_config mmu_config = {
