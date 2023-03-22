fb010938f29833e56e4e9ddaae6ccc5a570d435f
Mykola Kvach <mykola_kvach@epam.com>
2023-03-22 17:36:31 +0200

    soc: arm64: add support of r8a779f
    Add support of r8a779f.
    
    Add cpg driver for r8a779f with minimal functionality,
    it is possible to turn on/off only non-core timers.
    
    Add appropriate changes needed for PFC, but only related to MMC/SD.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

---
+++ soc/arm64/renesas_rcar/gen4/mmu_regions.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/arch/arm64/arm_mmu.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util.h>
+
+static const struct arm_mmu_region mmu_regions[] = {
+	MMU_REGION_FLAT_ENTRY("GIC",
+			      DT_REG_ADDR_BY_IDX(DT_INST(0, arm_gic), 0),
+			      DT_REG_SIZE_BY_IDX(DT_INST(0, arm_gic), 0),
+			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("GIC",
+			      DT_REG_ADDR_BY_IDX(DT_INST(0, arm_gic), 1),
+			      DT_REG_SIZE_BY_IDX(DT_INST(0, arm_gic), 1),
+			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
+
+#if DT_NODE_HAS_STATUS(DT_INST(0, renesas_r8a779f_cpg_mssr), okay)
+	MMU_REGION_FLAT_ENTRY("CPG",
+			      DT_REG_ADDR(DT_INST(0, renesas_r8a779f_cpg_mssr)),
+			      DT_REG_SIZE(DT_INST(0, renesas_r8a779f_cpg_mssr)),
+			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_INST(0, xen_xen), okay)
+	/* TODO: add mapping to a init driver function or use SYS_INIT for mapping this region */
+	MMU_REGION_FLAT_ENTRY("HYPERVISOR",
+			      DT_REG_ADDR_BY_IDX(DT_INST(0, xen_xen), 0),
+			      DT_REG_SIZE_BY_IDX(DT_INST(0, xen_xen), 0),
+			      MT_NORMAL | MT_P_RW_U_NA | MT_NS),
+#endif
+};
+
+const struct arm_mmu_config mmu_config = {
+	.num_regions = ARRAY_SIZE(mmu_regions),
+	.mmu_regions = mmu_regions,
+};
---
+++ soc/arm64/renesas_rcar/gen4/soc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#endif /* _SOC__H_ */
