f4f29108799989697de52d8de1250643cf5d65a3
Mykola Kvach <mykola_kvach@epam.com>
2023-04-05 01:17:42 +0300

    drivers: rcar_cpg: do MMIO mapping inside driver
    Add MMIO mapping for Renesas CPG driver in order to avoid
    mappings inside mmu_regions.c file. Remove MMU region for
    Renesas CPG driver.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- soc/arm64/renesas_rcar/gen3/mmu_regions.c
+++ soc/arm64/renesas_rcar/gen3/mmu_regions.c
@@ -17,11 +17,6 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_ADDR_BY_IDX(DT_INST(0, arm_gic), 1),
 			      DT_REG_SIZE_BY_IDX(DT_INST(0, arm_gic), 1),
 			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
-
-	MMU_REGION_FLAT_ENTRY("CPG",
-			      DT_REG_ADDR(DT_INST(0, renesas_r8a7795_cpg_mssr)),
-			      DT_REG_SIZE(DT_INST(0, renesas_r8a7795_cpg_mssr)),
-			      MT_DEVICE_nGnRnE | MT_RW | MT_NS),
 };
 
 const struct arm_mmu_config mmu_config = {
