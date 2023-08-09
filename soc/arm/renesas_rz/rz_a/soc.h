c7e976d1c15535b5a1362f50140027fc586b790e
Mykola Kvach <mykola_kvach@epam.com>
2023-08-09 11:08:47 +0300

    soc: arm: add support of R7S9210 SoC
    Add basic support of R7S9210 SoC.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

---
+++ soc/arm/renesas_rz/rz_a/soc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifndef _ASMLANGUAGE
+
+/*
+ * The following definitions are required for the inclusion of the CMSIS
+ * Common Peripheral Access Layer for aarch32 Cortex-A CPUs:
+ */
+
+#define __CORTEX_A 9U
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _SOC__H_ */
