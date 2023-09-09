907ffcb0ff39888173d6c85a98c30c102133975c
Mykola Kvach <mykola_kvach@epam.com>
2023-09-10 02:17:04 +0300

    drivers: pwm: add support of rz a2m general purpose pwm timer
    Add support of rz a2m general purpose pwm timer.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Reviewed-by: Dmytro Semenets <dmytro_semenets@epam.com>

---
+++ include/zephyr/dt-bindings/pwm/rza2m_pwm.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_RZA2M_PWM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_RZA2M_PWM_H_
+
+#define PWM_CHANNEL_INPUT_A (0 << 31)
+#define PWM_CHANNEL_INPUT_B (1 << 31)
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_RZA2M_PWM_H_ */
