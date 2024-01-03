4f7e410c8a89092c0dd969d2e3ee40a637bbf9bc
Dmytro Semenets <dmytro_semenets@epam.com>
2024-01-03 18:51:54 +0200

    soc: arm: add support of R9A08G045 SoC
    Add basic support of R9A08G045 SoC.
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

---
+++ soc/arm/renesas_rz/rz_g/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifndef _ASMLANGUAGE
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _SOC__H_ */
