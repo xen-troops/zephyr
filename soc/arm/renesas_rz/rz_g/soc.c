0afa3b9fa190e460c88f8a8657be05630ff9d0e0
Grygorii Strashko <grygorii_strashko@epam.com>
2024-01-12 17:51:11 +0200

    soc: arm: renesas_rz: rz_g: fix build warn
    The RZ G3S Zephyr core is Cortex-M33 so
    
    1) it does not have __set_VBAR(), and it handles vector_table relocation in
    z_arm_prep_c()
    
    2) it doesn't have MMU so remove related dead code.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- soc/arm/renesas_rz/rz_g/soc.c
+++ soc/arm/renesas_rz/rz_g/soc.c
@@ -22,12 +22,5 @@ static int soc_rz_init(void)
 
 SYS_INIT(soc_rz_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
 
-extern char _vector_start[];
-
-void relocate_vector_table(void)
-{
-	__set_VBAR(POINTER_TO_UINT(_vector_start));
-}
-
 /* Platform-specific early initialization */
 void z_arm_platform_init(void) {}
