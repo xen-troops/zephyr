1b09f7b5540b291e2afe97ea84acaf86a7fd57cd
Grygorii Strashko <grygorii_strashko@epam.com>
2024-01-15 11:55:05 +0200

    soc: arm: renesas: rzg: add proper arm cmsis cfg
    Add proper ARM CMSIS configuration for Cortex-M33 CPUs.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- soc/arm/renesas_rz/rz_g/soc.h
+++ soc/arm/renesas_rz/rz_g/soc.h
@@ -10,6 +10,14 @@
 
 #ifndef _ASMLANGUAGE
 
+/* __CM33_REV                0x0004U   Core revision r0p4 */
+#define __SAUREGION_PRESENT	CONFIG_CPU_HAS_ARM_SAU
+#define __VTOR_PRESENT		CONFIG_CPU_CORTEX_M_HAS_VTOR
+#define __MPU_PRESENT		CONFIG_CPU_HAS_ARM_MPU
+
+/* core0 - no FPU, core1 - FPU */
+#define __FPU_PRESENT             0U        /* FPU present */
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _SOC__H_ */
