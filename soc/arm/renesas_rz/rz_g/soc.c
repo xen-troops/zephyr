f873dc3deb44f9083733824a7b5ed4dda60de822
Grygorii Strashko <grygorii_strashko@epam.com>
2024-01-17 11:20:38 +0200

    soc: arm: renesas: rz_g: add multi-level irq handling support
    Add support for multi-level irq handling support for Renesas RZ G3S SoC
    where INTC IRQ controller is present in front of NVIC and used to configure
    pin IRQ0..7.
    
    This patch adds SoC interrupt control interface which dispatches IRQ manage
    calls between NVIC and INTC drivers.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- soc/arm/renesas_rz/rz_g/soc.c
+++ soc/arm/renesas_rz/rz_g/soc.c
@@ -3,17 +3,123 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/arch/cpu.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/init.h>
-#include <zephyr/sys/sys_io.h>
-#include <zephyr/sys/util.h>
 #include <cmsis_core.h>
 #include <zephyr/arch/arm/cortex_m/nvic.h>
 #include <zephyr/irq.h>
 #include <zephyr/irq_nextlevel.h>
 #include "soc.h"
+
+#ifdef CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER
+
+#define NUM_IRQS_PER_REG 32
+#define REG_FROM_IRQ(irq) ((irq) / NUM_IRQS_PER_REG)
+#define BIT_FROM_IRQ(irq) ((irq) % NUM_IRQS_PER_REG)
+
+void z_soc_irq_enable(unsigned int irq)
+{
+	const struct device *lvl2_intc;
+
+	switch (irq_get_level(irq)) {
+	case 1:
+		NVIC_EnableIRQ((IRQn_Type)irq);
+		break;
+	case 2:
+		lvl2_intc = DEVICE_DT_GET(DT_INST(0, renesas_r9a08g045_intc));
+		irq_enable_next_level(lvl2_intc, irq);
+		break;
+	default:
+		break;
+	}
+}
+
+void z_soc_irq_disable(unsigned int irq)
+{
+	const struct device *lvl2_intc;
+
+	switch (irq_get_level(irq)) {
+	case 1:
+		NVIC_DisableIRQ((IRQn_Type)irq);
+		break;
+	case 2:
+		lvl2_intc = DEVICE_DT_GET(DT_INST(0, renesas_r9a08g045_intc));
+		irq_disable_next_level(lvl2_intc, irq);
+		break;
+	default:
+		break;
+	}
+}
+
+int z_soc_irq_is_enabled(unsigned int irq)
+{
+	const struct device *lvl2_intc;
+
+	switch (irq_get_level(irq)) {
+	case 1:
+		return NVIC->ISER[REG_FROM_IRQ(irq)] & BIT(BIT_FROM_IRQ(irq));
+	case 2:
+		lvl2_intc = DEVICE_DT_GET(DT_INST(0, renesas_r9a08g045_intc));
+		return irq_line_is_enabled_next_level(lvl2_intc, irq);
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+static void z_arm_nvic_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
+{
+	/* The kernel may reserve some of the highest priority levels.
+	 * So we offset the requested priority level with the number
+	 * of priority levels reserved by the kernel.
+	 */
+
+	/* If we have zero latency interrupts, those interrupts will
+	 * run at a priority level which is not masked by irq_lock().
+	 * Our policy is to express priority levels with special properties
+	 * via flags
+	 */
+	if (IS_ENABLED(CONFIG_ZERO_LATENCY_IRQS) && (flags & IRQ_ZERO_LATENCY)) {
+		if (ZERO_LATENCY_LEVELS == 1) {
+			prio = _EXC_ZERO_LATENCY_IRQS_PRIO;
+		} else {
+			/* Use caller supplied prio level as-is */
+		}
+	} else {
+		prio += _IRQ_PRIO_OFFSET;
+	}
+
+	/* The last priority level is also used by PendSV exception, but
+	 * allow other interrupts to use the same level, even if it ends up
+	 * affecting performance (can still be useful on systems with a
+	 * reduced set of priorities, like Cortex-M0/M0+).
+	 */
+	__ASSERT(prio <= (BIT(NUM_IRQ_PRIO_BITS) - 1),
+		 "invalid priority %d for %d irq! values must be less than %lu\n",
+		 prio - _IRQ_PRIO_OFFSET, irq,
+		 BIT(NUM_IRQ_PRIO_BITS) - (_IRQ_PRIO_OFFSET));
+	NVIC_SetPriority((IRQn_Type)irq, prio);
+}
+
+void z_soc_irq_priority_set(unsigned int irq, unsigned int prio, unsigned int flags)
+{
+	const struct device *lvl2_intc;
+
+	switch (irq_get_level(irq)) {
+	case 1:
+		z_arm_nvic_irq_priority_set(irq, prio, flags);
+		break;
+	case 2:
+		lvl2_intc = DEVICE_DT_GET(DT_INST(0, renesas_r9a08g045_intc));
+		irq_set_priority_next_level(lvl2_intc, irq, prio, flags);
+		break;
+	default:
+		break;
+	}
+}
+
+#endif /* CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER */
 
 static int soc_rz_init(void)
 {
