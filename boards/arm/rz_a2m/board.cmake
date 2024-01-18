94137cb2991003b9e9682bb1b828f1b76d88bf5b
Dmytro Semenets <dmytro_semenets@epam.com>
2024-01-18 19:30:53 +0200

    boards: rz_a2m: Add build target for create release notes
    Add build target "doc" to build release notes.
    Command line:
     west build -t doc
    This command produces file build/zephyr/rza2m_release_notes.pdf
    with release notes.
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- boards/arm/rz_a2m/board.cmake
+++ boards/arm/rz_a2m/board.cmake
@@ -2,3 +2,9 @@
 
 board_runner_args(jlink "--device=R7S921053VCBG" "--speed=15000")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+set(RELEASE_NOTES_SRC rza2m_release_notes.tex)
+add_custom_target(doc
+    WORKING_DIRECTORY ${ZEPHYR_BASE}/boards/${ARCH}/${BOARD}/release_notes
+    COMMAND pdflatex -output-directory=${ZEPHYR_BINARY_DIR} ${RELEASE_NOTES_SRC}
+)
