1a88ad0d311632a4fb446770b8e36f953403ab6c
Grygorii Strashko <grygorii_strashko@epam.com>
2024-01-30 18:39:44 +0200

    dts: bindings: gpio: renesas: add rzg3s gpio bindings
    Add DT bindings for Renesas RZ G3S SoC GPIO controller.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

---
+++ include/zephyr/dt-bindings/gpio/rzg3s-gpio.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_RZG3S_GPIO_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_RZG3S_GPIO_H_
+
+/**
+ * @brief RZ G3S specific GPIO Flags
+ *
+ * The pin driving ability flags are encoded in the 8 upper bits of @ref gpio_dt_flags_t as
+ * follows:
+ *
+ * - Bit 8: Pin driving ability present
+ * - Bit 10..9: Pin driving ability value using PINCTRL_RZG3S_PIN_IOLH_xx macro as specified in
+ *              dt-bindings/pinctrl/renesas/pinctrl-r9a08g045.h
+ *
+ *   Use RZA2_GPIO_DRIVE_IOLH_SET(iolh_val) macro for the GPIO pin driving ability configuration.
+ *
+ * @ingroup gpio_interface
+ * @{
+ */
+
+/**
+ * @name RZ G3S pin driving ability flags
+ * @brief RZ G3S pin driving ability flags
+ * @{
+ */
+
+/** @cond INTERNAL_HIDDEN */
+/**  Pin driving ability value shift */
+#define RZA2_GPIO_DRIVE_IOLH_SHIFT 9U
+#define RZA2_GPIO_DRIVE_IOLH_MASK  (0x3 << RZA2_GPIO_DRIVE_IOLH_SHIFT)
+
+/** @endcond */
+
+/**  Pin driving ability present */
+#define RZA2_GPIO_DRIVE_IOLH_PRESENT BIT(8)
+
+/**  Pin driving ability configuration macro */
+#define RZA2_GPIO_DRIVE_IOLH_SET(iolh_val)                                                         \
+	(RZA2_GPIO_DRIVE_IOLH_PRESENT | ((iolh_val) << RZA2_GPIO_DRIVE_IOLH_SHIFT)))
+
+#define RZA2_GPIO_DRIVE_IOLH_GET(flags)                                                            \
+	(((flags) & RZA2_GPIO_DRIVE_IOLH_MASK) >> RZA2_GPIO_DRIVE_IOLH_SHIFT)
+
+/** @} */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_RZG3S_GPIO_H_ */
