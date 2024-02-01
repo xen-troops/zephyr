15bc37aec2ea4f39fe47d38823772c37ec18dafe
Grygorii Strashko <grygorii_strashko@epam.com>
2024-02-01 22:25:56 +0200

    soc: arm: renesas: rzg3s: update arm cmsis cfg
    Update ARM CMSIS to use Kconfig defines for all options.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Acked-by: Dmytro Semenets <dmytro_semenets@epam.com>

--- soc/arm/renesas_rz/rz_g/soc.h
+++ soc/arm/renesas_rz/rz_g/soc.h
@@ -10,13 +10,16 @@
 
 #ifndef _ASMLANGUAGE
 
-/* __CM33_REV                0x0004U   Core revision r0p4 */
-#define __SAUREGION_PRESENT	CONFIG_CPU_HAS_ARM_SAU
-#define __VTOR_PRESENT		CONFIG_CPU_CORTEX_M_HAS_VTOR
-#define __MPU_PRESENT		CONFIG_CPU_HAS_ARM_MPU
-
-/* core0 - no FPU, core1 - FPU */
-#define __FPU_PRESENT             0U        /* FPU present */
+/* __CM33_REV                0x0004U   Core revision r0p4
+ * M33 core0 - no FPU, M33 core1 - with FPU
+ */
+#define __SAUREGION_PRESENT CONFIG_CPU_HAS_ARM_SAU
+#define __VTOR_PRESENT      CONFIG_CPU_CORTEX_M_HAS_VTOR
+#define __MPU_PRESENT       CONFIG_CPU_HAS_ARM_MPU
+#define __FPU_PRESENT       CONFIG_CPU_HAS_FPU /* FPU present */
+#define __DSP_PRESENT       CONFIG_ARMV8_M_DSP /* DSP ext present - no */
+#define __DCACHE_PRESENT    CONFIG_CPU_HAS_DCACHE /* DCACHE present - no */
+#define __ICACHE_PRESENT    CONFIG_CPU_HAS_ICACHE /* ICACHE present - no */
 
 #endif /* !_ASMLANGUAGE */
 
