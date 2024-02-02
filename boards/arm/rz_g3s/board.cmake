f1a4be6e2399627da5119afccff6ce636d8d6038
Grygorii Strashko <grygorii_strashko@epam.com>
2024-02-02 14:57:54 +0200

    dts: arm: renesas: r9a08g045: add configuration for cortex-m33_fpu
    The Renesas RZ G3S SoC has two System CPUs Cortex-M33/Cortex-M33_FPU. The
    differences between them are:
    - that Cortex-M33_FPU has Floating-Point Unit (FPU) implemented
    - INTC, TINT addreses are different.
    - can have different flash, (s)ram layout
    
    It is not a SMP capable SoC.
    
    So two rz_g3s boards were created:
    1) rz_g3s to build on M33 with no FPU core/
    This can be built using the following commnand:
        west build -b rz_g3s -p always <APP_PATH>
    2) rz_g3s_fpu to build on M33 with FPU core.
    This can be built using command:
        west build -b rz_g3s_fpu -p always <APP_PATH>
    
    rz_g3s_fpu does the following changes comparing to rz_g3s
    board:
     - change CPU  compatible = "arm,cortex-m33f"
     - update INTC, TINT addresses
     - adjust flash, (s)ram layout if required.
     - enable FPU and FPU_SHARING support
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Reviewed-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Tested-by: Dmytro Semenets <dmytro_semenets@epam.com>

--- boards/arm/rz_g3s/board.cmake
+++ boards/arm/rz_g3s/board.cmake
@@ -1,10 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=R9A08G045S33_M33_0" "--speed=15000")
+if (CONFIG_SOC_SERIES_RZ_G_ARM_FPU)
+	board_runner_args(jlink "--device=R9A08G045S33_M33_1" "--speed=15000")
+elseif(CONFIG_SOC_SERIES_RZ_G_ARM)
+	board_runner_args(jlink "--device=R9A08G045S33_M33_0" "--speed=15000")
+endif ()
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
 set(RELEASE_NOTES_SRC index.rst)
 set(RELEASE_NOTES_OUT rzg3s_release_notes.pdf)
+
 add_custom_target(doc
     WORKING_DIRECTORY ${ZEPHYR_BASE}/boards/${ARCH}/${BOARD}/doc
     COMMAND rst2pdf ${RELEASE_NOTES_SRC} -o ${ZEPHYR_BINARY_DIR}/${RELEASE_NOTES_OUT}
