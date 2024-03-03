c958d9eb2be95a9d925109d0b9a2a6c18a1bbae5
Dmytro Semenets <dmytro_semenets@epam.com>
2024-03-03 18:05:34 +0200

    boards: rz_g3s: Generate SREC file after build
    Generate SREC file after build. File will be placed in
    <build>/zephyr directory
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- boards/arm/rz_g3s/board.cmake
+++ boards/arm/rz_g3s/board.cmake
@@ -10,10 +10,16 @@ include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 set(RELEASE_NOTES_SRC .)
 set(RELEASE_NOTES_OUT rzg3s_release_notes.pdf)
 
+set(KERNEL_SREC_NAME   ${KERNEL_NAME}.srec)
+
 add_custom_target(doc
     WORKING_DIRECTORY ${ZEPHYR_BASE}/boards/${ARCH}/${BOARD}/doc
     COMMAND sphinx-build -M latexpdf ${RELEASE_NOTES_SRC} ${ZEPHYR_BINARY_DIR}
     COMMAND cp ${ZEPHYR_BINARY_DIR}/latex/rzg3s_release_notes.pdf  ${ZEPHYR_BINARY_DIR}/${RELEASE_NOTES_OUT}
     COMMAND rm -Rf ${ZEPHYR_BINARY_DIR}/latex
     COMMAND rm -Rf ${ZEPHYR_BINARY_DIR}/doctrees
+)
+
+set_property(GLOBAL APPEND PROPERTY extra_post_build_commands
+    COMMAND ${CMAKE_OBJCOPY} -O srec --srec-forceS3 ${ZEPHYR_BINARY_DIR}/${KERNEL_ELF_NAME} ${ZEPHYR_BINARY_DIR}/${KERNEL_SREC_NAME}
 )
