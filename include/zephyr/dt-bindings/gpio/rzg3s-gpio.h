1853820befbeeb6a89a100e9c0562085d2404e25
Grygorii Strashko <grygorii_strashko@epam.com>
2024-03-05 13:26:13 +0200

    drivers: gpio: rzg3s: add support for digital noise filter
    This patch adds support for RZ G3S Digital Noise Filter configuration
    (debounce) for pins configured in GPIO mode. Hence, Zephyr doesn't have
    generic support for debounce in GPIO API this patch adds support for
    Digital Noise Filter by using custom GPIO DT flags:
    
     - Bit 11: Pin Digital Noise Filter ability present
     - Bit 13..12: Digital Noise Filter Number value
     - Bit 15..14: Digital Noise Filter Clock Selection value
    
    The Digital Noise Filter configuration should be defined using macro
    RZG3S_GPIO_FILTER_SET(filnum, filclksel), where filnum is
    FILNUM_m.FILNUM_mX field value and filclksel is FILCLKSEL_m.FILCLKSEL_mX
    field value.
    
    example:
     out-gpios = <&port8 2 (RZG3S_GPIO_FILTER_SET(3, 3))>;
    
    With 24Mhz external clock:
     - min debounce time will be 166.666ns for FILNUM_mX=0 and FILCLKSEL_mX=0
     - max debounce time will be 24ms for FILNUM_mX=3 and FILCLKSEL_mX=3
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Reviewed-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- include/zephyr/dt-bindings/gpio/rzg3s-gpio.h
+++ include/zephyr/dt-bindings/gpio/rzg3s-gpio.h
@@ -18,6 +18,10 @@
  *
  *   Use RZG3S_GPIO_DRIVE_IOLH_SET(iolh_val) macro for the GPIO pin driving ability configuration.
  *
+ * - Bit 11: Pin Digital Noise Filter ability present
+ * - Bit 13..12: Digital Noise Filter Number value
+ * - Bit 15..14: Digital Noise Filter Clock Selection value
+ *
  * @ingroup gpio_interface
  * @{
  */
@@ -33,10 +37,21 @@
 #define RZG3S_GPIO_DRIVE_IOLH_SHIFT 9U
 #define RZG3S_GPIO_DRIVE_IOLH_MASK  (0x3 << RZG3S_GPIO_DRIVE_IOLH_SHIFT)
 
+/**  Pin Digital Noise Filter Number */
+#define RZG3S_GPIO_FILTER_NUM_SHIFT 12U
+#define RZG3S_GPIO_FILTER_NUM_MASK  (0x3 << RZG3S_GPIO_FILTER_NUM_SHIFT)
+
+/**  Pin Digital Noise Filter Clock Selection */
+#define RZG3S_GPIO_FILTER_CLK_SHIFT 14U
+#define RZG3S_GPIO_FILTER_CLK_MASK  (0x3 << RZG3S_GPIO_FILTER_CLK_SHIFT)
+
 /** @endcond */
 
 /**  Pin driving ability present */
 #define RZG3S_GPIO_DRIVE_IOLH_PRESENT BIT(8)
+
+/**  Pin Digital Noise Filter present */
+#define RZG3S_GPIO_FILTER_PRESENT BIT(11)
 
 /**  Pin driving ability configuration macro */
 #define RZG3S_GPIO_DRIVE_IOLH_SET(iolh_val)                                                        \
@@ -45,6 +60,16 @@
 #define RZG3S_GPIO_DRIVE_IOLH_GET(flags)                                                           \
 	(((flags) & RZG3S_GPIO_DRIVE_IOLH_MASK) >> RZG3S_GPIO_DRIVE_IOLH_SHIFT)
 
+/**  Pin Digital Noise Filter ability configuration macro */
+#define RZG3S_GPIO_FILTER_SET(filnum, filclksel)                                                   \
+	(RZG3S_GPIO_FILTER_PRESENT | ((filnum) << RZG3S_GPIO_FILTER_NUM_SHIFT) |                   \
+	 ((filclksel) << RZG3S_GPIO_FILTER_CLK_SHIFT))
+
+#define RZG3S_GPIO_FILTER_NUM_GET(flags)                                                           \
+	(((flags) & RZG3S_GPIO_FILTER_NUM_MASK) >> RZG3S_GPIO_FILTER_NUM_SHIFT)
+
+#define RZG3S_GPIO_FILTER_CLK_GET(flags)                                                           \
+	(((flags) & RZG3S_GPIO_FILTER_CLK_MASK) >> RZG3S_GPIO_FILTER_CLK_SHIFT)
 
 /** @} */
 
